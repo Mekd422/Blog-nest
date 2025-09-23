5e1545997041098c6027011be1af3a975cf05c08
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-09-23 22:32:11 +0300

    tag module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TagController } from './tag/tag.controller';
+import { TagService } from './tag/tag.service';
 
 @Module({
-  imports: [],
+  imports: [TagController, TagService],
   controllers: [AppController],
   providers: [AppService],
 })
