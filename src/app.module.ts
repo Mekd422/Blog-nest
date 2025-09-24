b6d9db89f95d01f7bc2a1c4d7e2ba19ddc6fd534
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-09-24 11:13:13 +0300

    |

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TagController } from './tag/tag.controller';
-import { TagService } from './tag/tag.service';
+import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from './ormconfig';
 
 @Module({
-  imports: [TagController, TagService],
+  imports: [TagModule, TypeOrmModule.forRoot(ormconfig)],
   controllers: [AppController],
   providers: [AppService],
 })
