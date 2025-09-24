9eb5b3674fcce8c002c4bbc4b27390d752b43c5c
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-09-24 13:05:36 +0300

    |

---
+++ src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { CreateUserDto } from '@/user/createUser.dto';
+import { UserService } from '@/user/user.service';
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post()
+  createUser(@Body('user') createUserDto: CreateUserDto): any {
+    return this.userService.createUser(createUserDto);
+  }
+}
