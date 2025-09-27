87f35e8ef6df9fb5fd65965a78362865c36d4251
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-09-27 15:07:58 +0300

    |

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,12 @@
 import { CreateUserDto } from '@/user/createUser.dto';
+import { IUserResponse } from '@/user/types/UserResponse.Interface';
 import { UserEntity } from '@/user/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import {sign} from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 @Injectable()
 export class UserService {
@@ -10,11 +14,32 @@ export class UserService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
   ) {}
-  async createUser(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+  async createUser(createUserDto: CreateUserDto): Promise<IUserResponse> {
     console.log('Received DTO:', createUserDto);
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
 
-    return await this.userRepository.save(newUser);
+    const SavedUser = await this.userRepository.save(newUser);
+
+    return this.generateUserResponse(SavedUser);
+  }
+
+  generateToken(user: UserEntity) : String{
+    return sign({
+      id: user.id,
+      username: user.username,
+      email: user.email
+    },
+    process.env.JWT_SECRET_KEY,
+  )
+  }
+
+  generateUserResponse(user: UserEntity) : IUserResponse {
+    return {
+      user:{
+        ...user,
+        token: this.generateToken(user)
+      }
+    }
   }
 }
