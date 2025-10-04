7ffbd4cd68a41d6fada499d429a180a0d14d2dc4
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-10-04 21:43:42 +0300

    add auth middleware and get current user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,30 +2,41 @@ import { CreateUserDto } from '@/user/dto/createUser.dto';
 import { LoginDto } from '@/user/dto/loginUser.dto';
 import { IUserResponse } from '@/user/types/UserResponse.Interface';
 import { UserService } from '@/user/user.service';
+import type { AuthRequest } from '@/types/expressRequest.interface';
+
+import { Req } from "@nestjs/common";
+
 import {
   Body,
   Controller,
   Post,
+  Get,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 
-@Controller('users')
+@Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @UsePipes(new ValidationPipe())
-  @Post()
+  @Post("users")
   createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<IUserResponse> {
     return this.userService.createUser(createUserDto);
   }
 
-  @Post('login')
+  @Post('users/login')
   @UsePipes(new ValidationPipe())
   async loginUser(@Body('user') LoginDto: LoginDto): Promise<IUserResponse> {
     const user = await this.userService.LoginUser(LoginDto);
     return this.userService.generateUserResponse(user);
+  }
+
+  @Get('user')
+  async getCurrentUser(@Req() request: AuthRequest): Promise<IUserResponse> {
+    console.log(request.user);
+    return this.userService.generateUserResponse(request.user);
   }
 }
